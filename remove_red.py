eded8139a5304244778ad95d504b021a05f6ddb4
amag1 <amaglione2002@gmail.com>
2025-06-08 16:25:28 -0300

    Fix remove red function

--- remove_red.py
+++ remove_red.py
@@ -1,9 +1,19 @@
-import cv2
-
-
-def remove_red(img_bgr, r_thresh=200, gb_thresh=50):
+def remove_red(img_bgr):
     """Pone en negro los píxeles predominantemente rojos de una imagen BGR."""
-    b, g, r = cv2.split(img_bgr)
-    red_mask = (r > r_thresh) & (g < gb_thresh) & (b < gb_thresh)
-    img_bgr[red_mask] = (0, 0, 0)  # negro en BGR
-    return img_bgr
+    out = img_bgr.copy()
+
+    r = out[..., 0]
+    g = out[..., 1]
+    b = out[..., 2]
+
+    # mask definitions
+    red_only = (r > 0) & (g == 0) & (b == 0)
+    green_only = (g > 0) & (r == 0) & (b == 0)
+    blue_only = (b > 0) & (r == 0) & (g == 0)
+
+    # apply transformations
+    out[red_only] = [255, 255, 255]
+    out[green_only] = [255, 255, 255]
+    out[blue_only] = [0, 0, 0]
+
+    return out
